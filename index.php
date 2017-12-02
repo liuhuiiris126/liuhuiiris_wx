e8e25ee1119429e0d488a15e9588cfe7fb6de9da
liuhui <liuhuiiris@126.com>
2017-12-02 12:26:51 +0800

    自动回复测试

--- index.php
+++ index.php
@@ -1,37 +1,66 @@
 <?php
-/*-----------------------------------------------
-|   index.php [ 微信公众平台接口 ]
-+--------------------------------------------------
-|   Author: LimYoonPer
-+------------------------------------------------*/
+define("TOKEN", "weixin");
 $wechatObj = new wechat();
-$wechatObj->responseMsg();
-class wechat {
- public function responseMsg() {
-  //---------- 接 收 数 据 ---------- //
-  $postStr = $GLOBALS["HTTP_RAW_POST_DATA"]; //获取POST数据
-  //用SimpleXML解析POST过来的XML数据
-  $postObj = simplexml_load_string($postStr,'SimpleXMLElement',LIBXML_NOCDATA);
-  $fromUsername = $postObj->FromUserName; //获取发送方帐号（OpenID）
-  $toUsername = $postObj->ToUserName; //获取接收方账号
-  $keyword = trim($postObj->Content); //获取消息内容
-  $time = time(); //获取当前时间戳
-  //---------- 返 回 数 据 ---------- //
-  //返回消息模板
-  $textTpl = "<xml>
-  <ToUserName><![CDATA[%s]]></ToUserName>
-  <FromUserName><![CDATA[%s]]></FromUserName>
-  <CreateTime>%s</CreateTime>
-  <MsgType><![CDATA[%s]]></MsgType>
-  <Content><![CDATA[%s]]></Content>
-  <FuncFlag>0</FuncFlag>
-  </xml>";
-  $msgType = "text"; //消息类型
-  include('simsimi.php');
-  $contentStr = simsimi($keyword); //返回消息内容
-  //格式化消息模板
-  $resultStr = sprintf($textTpl,$fromUsername,$toUsername,$time,$msgType,$contentStr);
-  echo $resultStr; //输出结果
- }
+if (isset($_GET['echostr'])) {
+  $wechatObj->valid();
+}else{
+  $wechatObj->responseMsg();
+}
+class wechat
+{
+  public function valid()
+  {
+    $echoStr = $_GET["echostr"];
+    if($this->checkSignature()){
+      header('content-type:text');
+      echo $echoStr;
+      exit;
+    }
+  }
+  private function checkSignature()
+  {
+    $signature = $_GET["signature"];
+    $timestamp = $_GET["timestamp"];
+    $nonce = $_GET["nonce"];
+    $token = TOKEN;
+    $tmpArr = array($token, $timestamp, $nonce);
+    sort($tmpArr, SORT_STRING);
+    $tmpStr = implode( $tmpArr );
+    $tmpStr = sha1( $tmpStr );
+    if( $tmpStr == $signature ){
+      return true;
+    }else{
+      return false;
+    }
+  }
+  public function responseMsg()
+  {
+    $postStr = $GLOBALS["HTTP_RAW_POST_DATA"];
+    if (!empty($postStr)){
+      $postObj = simplexml_load_string($postStr, 'SimpleXMLElement', LIBXML_NOCDATA); //获取数据
+      $fromUsername = $postObj->FromUserName;
+      $toUsername = $postObj->ToUserName;
+      $keyword = trim($postObj->Content);
+      $time = time();
+      $textTpl = "<xml>
+            <ToUserName><![CDATA[%s]]></ToUserName>
+            <FromUserName><![CDATA[%s]]></FromUserName>
+            <CreateTime>%s</CreateTime>
+            <MsgType><![CDATA[%s]]></MsgType>
+            <Content><![CDATA[%s]]></Content>
+            <FuncFlag>0</FuncFlag>
+            </xml>";
+      if($keyword == "?" || $keyword == "？") //获取用户信息
+      {
+        $msgType = "text";
+        $contentStr = date("Y-m-d H:i:s",time()); // 回复的内容
+        $resultStr = sprintf($textTpl, $fromUsername, $toUsername, $time, $msgType, $contentStr);
+        echo $resultStr;
+      }
+    }else{
+      echo "";
+      exit;
+    }
+  }
 }
 ?>
